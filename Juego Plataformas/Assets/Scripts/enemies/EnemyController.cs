ca0056432e4ebcc1b2cfdf274b0de5e65afea112
AlucardTheMaster <kakauleite97@gmail.com>
2018-05-26 17:05:29 +0200

    Añadiendo Golem
    creando objeto Golem, con animaciones Walk y Attack. Pero hay problemas con TODO!! Quiza no se use al final

--- Juego Plataformas/Assets/Scripts/enemies/EnemyController.cs
+++ Juego Plataformas/Assets/Scripts/enemies/EnemyController.cs
@@ -8,7 +8,7 @@ public class EnemyController : MonoBehaviour {
     public float maxSpeed = 1f;
     public float speed = 1f;
 
-    public Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
 
     // Use this for initialization
     void Start () {
