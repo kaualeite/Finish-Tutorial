d7d12df7223751393fdc1c033da6ff4b2ec18b20
Pol Galbarro <polgalbarroaloy@gmail.com>
2018-05-31 13:40:26 +0200

    light bullet

---
+++ Juego Plataformas/Assets/Scripts/Items/Bullets 1/impulseRight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class impulseRight : MonoBehaviour {
+
+    public GameObject obj;
+    public float vx = 0;
+    public float vy = 0;
+    private Rigidbody2D rb2d;
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(new Vector2(vx, vy));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
