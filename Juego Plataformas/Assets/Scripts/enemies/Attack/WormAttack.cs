1a9f00f718a16f950f93e2c94031bf3a1d892e75
AlucardTheMaster <kakauleite97@gmail.com>
2018-06-01 23:25:29 +0200

    Creando enemigo Gusano

---
+++ Juego Plataformas/Assets/Scripts/enemies/Attack/WormAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormAttack : MonoBehaviour {
+
+    public Collider2D attackTrigger;
+    public int damage;
+
+    void Awake()
+    {
+        attackTrigger.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Comprobación de que es el jugador quien interactua con el
+        if (col.gameObject.name == "Player")
+        {
+            attackTrigger.enabled = true;
+            col.SendMessage("EnemyKnockBack", transform.position.x);
+            col.SendMessage("Hit", damage);
+        }
+    }
+}
