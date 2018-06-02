7cfe78f1ecd3cb436c60a764d9068cc333046f81
AlucardTheMaster <kakauleite97@gmail.com>
2018-06-02 18:59:34 +0200

    Creando script veneno

---
+++ Juego Plataformas/Assets/Veneno.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Veneno : MonoBehaviour {
+
+    public int damage;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Comprobación de que es el jugador quien interactua con el
+        if (col.gameObject.name == "Player")
+        {
+            col.SendMessage("EnemyKnockBack", transform.position.x);
+            col.SendMessage("Hit", damage);
+        }
+    }
+}
