80b68c1f679448ddca9ee2c3ae1a467813ca423d
polsafa <nork_pol@hotmail.es>
2018-06-06 01:36:57 +0200

    bolotes hermanaco

---
+++ Juego Plataformas/Assets/Scripts/Items/Bullets 1/RockDealDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDealDamage : MonoBehaviour {
+    public int damage;
+    public PlayerController player;
+    void Start()
+    {
+        player = (GameObject.Find("Player").GetComponent(typeof(PlayerController)) as PlayerController);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            player.EnemyKnockBack(15);
+            player.Hit(damage);
+            Destroy(GameObject.Find(this.gameObject.name));
+
+        }
+    }
+}
