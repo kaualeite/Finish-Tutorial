d906d2b210e52db0e3fe67cbd2ff866190431efa
Pol Galbarro <polgalbarroaloy@gmail.com>
2018-06-05 11:33:23 +0200

    adding reaload data.... solving some problems

--- Juego Plataformas/Assets/Scripts/canvas/MainMenu_to_firstscene.cs
+++ Juego Plataformas/Assets/Scripts/canvas/MainMenu_to_firstscene.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
+
 
 public class MainMenu_to_firstscene : MonoBehaviour {
 
     public GameObject menu;
+    private string filename = "data.txt";
+    public string escena = "";
+    public float posx = 0;
+    public float posy = 0;
+
 	// Use this for initialization
 	void Start () {
         menu.SetActive(true);
@@ -18,11 +25,12 @@ public class MainMenu_to_firstscene : MonoBehaviour {
     public void continueGame()
     {
 
-        SceneManager.LoadScene("nuevaScene", LoadSceneMode.Single);
+        read();
+        SceneManager.LoadScene(escena, LoadSceneMode.Single);
     }
 	public void newGame(){
 
-        SceneManager.LoadScene("nuevaScene", LoadSceneMode.Single);
+        SceneManager.LoadScene("first", LoadSceneMode.Single);
 	}
 
 	public void Quit(){
@@ -30,4 +38,33 @@ public class MainMenu_to_firstscene : MonoBehaviour {
 		Application.Quit();
 
 	}
+ 
+
+    public void read()
+    {
+        StreamReader reader = new StreamReader("./Save.txt");
+        string itemStrings = reader.ReadLine();
+        char[] delimiter = {';' };
+        string[] fields = itemStrings.Split(delimiter);
+        while (itemStrings != null)
+        {
+           
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+             
+                Debug.Log(fields[i]);
+
+
+            }
+
+            itemStrings = reader.ReadLine();
+         
+
+        }
+        escena = fields[0];
+        posx = float.Parse(fields[1]);
+        posy = float.Parse(fields[2]);
+    }
 }
+
