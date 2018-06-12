fbc0d59272e49f8a06ce2f4bc2cbddad630b4534
Pol Galbarro <polgalbarroaloy@gmail.com>
2018-06-12 12:43:06 +0200

    subo

---
+++ Juego Plataformas/Assets/explo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explo : MonoBehaviour {
+
+	// Use this for initialization
+    private bool attacking = false;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Comprobación de que es el jugador quien interactua con el
+        if (col.gameObject.tag == "Player")
+        {
+            attacking = true;
+            anim.SetBool("Explode", attacking);
+        }
+
+
+    }
+}
+
