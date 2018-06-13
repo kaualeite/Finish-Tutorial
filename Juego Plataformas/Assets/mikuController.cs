827281bc8f6e0ab88b792bae4718b10b6fdd89a5
Pol Galbarro <polgalbarroaloy@gmail.com>
2018-06-13 11:39:58 +0200

    add miku controller

---
+++ Juego Plataformas/Assets/mikuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mikuController : MonoBehaviour {
+    public int mikuHealth;
+    public int mikuMaxhealth = 5000;
+    public bool atackt2 = false;
+	// Use this for initialization
+	void Start () {
+        mikuHealth = mikuMaxhealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(mikuHealth == mikuMaxhealth/2){
+            atackt2 = true;
+        }
+	}
+}
